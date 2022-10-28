795eba174588f89e1b0c788f5440861aa1bb0c80
pier <piergiuseppe.mallozzi@gmail.com>
2022-10-28 10:41:14 -0700

    added new dockerfile

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+build_command = "docker buildx build --platform linux/amd64,linux/arm64 -f"
+
 main() {
 
     local devenv=${1?Need command}
 
     case "${devenv}" in
 
-        base-gui | base-gui-38 | base-gui-38-gymphysics )
-            docker build -f ./dockerfiles/${devenv}.dockerfile -t pmallozzi/devenvs:${devenv} . --no-cache
+        base-310 | base-gui | base-gui-38 | base-gui-38-gymphysics )
+            $build_command ./dockerfiles/${devenv}.dockerfile -t pmallozzi/devenvs:${devenv} --push . --no-cache
+#            docker build -f ./dockerfiles/${devenv}.dockerfile -t pmallozzi/devenvs:${devenv} . --no-cache
             ;;
         all )
             docker build -f ./dockerfiles/base-gui.dockerfile -t pmallozzi/devenvs:base-gui --no-cache .
