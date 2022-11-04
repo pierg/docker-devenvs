fff161c78f79cca15c8e6af1e174c902cc76772c
pier <piergiuseppe.mallozzi@gmail.com>
2022-11-04 11:16:14 -0700

    updated

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+
 function build_and_load(){
-  docker buildx build --platform linux/amd64,linux/arm64 -f ./dockerfiles/${1}.dockerfile -t pmallozzi/devenvs:${1} --push . --no-cache
-  docker buildx build --load -t pmallozzi/devenvs:${1} .
+  docker buildx build --push --platform linux/amd64,linux/arm64 -f ./dockerfiles/${1}.dockerfile -t pmallozzi/devenvs:${1} --push . --no-cache
 }
 
 main() {
@@ -20,9 +20,9 @@ main() {
             build_and_load "base-gui-gymphysics-310"
             ;;
         *)
-            echo "Unknown command: ${cmd}"
+            echo "Unknown command: ${devenv}"
             ;;
     esac
 }
 
-main $@
+main "$@"
